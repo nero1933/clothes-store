19b26b2bcf67c38a00441b33eb4299fe577efc8d
nero1933 <neroigo.1933@gmail.com>
2025-01-14 11:13:06 +0200

    UPDATE: Minor

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9-alpine3.19
+FROM python:3.11-alpine3.21
 LABEL maintainer="nero"
 
 ENV PYTHONUNBUFFERED 1
--- app/ecommerce/tasks/send_email.py
+++ app/ecommerce/tasks/send_email.py
@@ -1,12 +1,9 @@
 from celery import shared_task
 
-from django.core.checks import Error
-from django.core.mail import EmailMultiAlternatives, send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
 from django.conf import settings
-
-from ecommerce.models import UserProfile
 
 
 @shared_task
