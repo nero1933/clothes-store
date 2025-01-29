fcd6455df342505ca29e64c2f6c8edd3ba7b8931
nero1933 <neroigo.1933@gmail.com>
2025-01-29 11:05:13 +0200

    ADD: nested_admin

--- app/app/settings.py
+++ app/app/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django_filters',
     'phonenumber_field',
     'django_celery_beat',
+    'nested_admin',
 
     # apps
     'ecommerce.apps.EcommerceConfig',
--- app/app/urls.py
+++ app/app/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/v1/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
+    path('_nested_admin/', include('nested_admin.urls')),
     path('', include('ecommerce.urls')),
 ]
 
