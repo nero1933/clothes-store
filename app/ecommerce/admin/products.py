5cf38b502f40155daa33b7a8c3c405e03e18f8a6
nero1933 <neroigo.1933@gmail.com>
2025-01-30 12:09:34 +0200

    UPDATE: Products admin

--- app/ecommerce/admin/products.py
+++ app/ecommerce/admin/products.py
@@ -1,73 +1,85 @@
+import nested_admin
+
+from django import forms
 from django.contrib import admin
 from django.db.models import Prefetch, Avg
+from django.shortcuts import get_object_or_404
 
 from ecommerce.models import Review
 from ecommerce.models.products import *
 
 
-class ProductVariationInline(admin.TabularInline):
+class ProductVariationInline(admin.StackedInline):
     model = ProductVariation
     extra = 1
 
-class ProductItemInline(admin.TabularInline):
+
+class ProductItemForm(forms.ModelForm):
+    """ ProductItem Model Form """
+
+    discounts_choices = []
+    for item in Discount.objects.values('pk', 'name', 'discount_rate'):
+        discounts_choices.append(
+         (item['pk'], f"{item['name']} - {item['discount_rate']}%")
+        )
+    discount = forms.MultipleChoiceField(
+        required=True,
+        choices=discounts_choices
+    )
+
+    colors_choices = [('', '---------')]
+    colors_choices.extend(Color.objects.values_list('pk', 'name'))
+    color = forms.ChoiceField(
+        required=False,
+        choices=colors_choices
+    )
+
+    class Media:
+        css = {
+            'all': ('css/admin_styles.css',)  # Load custom CSS
+        }
+
+
+class ProductItemInline(admin.StackedInline):
     model = ProductItem
+    form = ProductItemForm
     extra = 1
-    inlines = [ProductVariationInline]
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'category', 'brand', 'gender', 'total_reviews')
-    list_display_links = ('name', )
-    inlines = [ProductItemInline]
-    # inlines = [ReviewInline]
-
-    def total_reviews(self, obj):
-        if obj.product_rating:
-            return round(obj.product_rating, 1)
-        else:
-            return 0
-
-    total_reviews.short_description = 'Reviews'
 
     def get_queryset(self, request):
-        review_queryset = Review.objects.only('id', 'product_id', 'rating', )
-
-        queryset = Product.objects \
-            .select_related(
-                'category',
-                'brand'
-            ) \
-            .prefetch_related(
-                Prefetch('review', queryset=review_queryset),
-                'product_item__product_variation',
-            ) \
-            .annotate(product_rating=Avg('review__rating'))
+        queryset = ProductItem.objects.select_related(
+            'product',
+            'color',
+        ).prefetch_related(
+            Prefetch('discount', queryset=Discount.objects.all()),
+        )
 
         return queryset
 
-    def get_object(self, request, object_id, from_field=None):
-        queryset = Product.objects \
-            .select_related(
-                'category',
-                'brand'
-            ) \
-            .prefetch_related(
-                'product_item',
-                'product_item__color',
-                'product_item__image',
-                'product_item__discount',
-                'product_item__product_variation',
-                'product_item__product_variation__size',
-                'product_item__product_variation__order_item',
-                'attribute_option',
-                'attribute_option__attribute_type',
-                'review',
-            ) \
 
-        try:
-            return queryset.get(pk=object_id)
-        except queryset.model.DoesNotExist:
-            return None
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ('product',)
+    list_select_related = True
+    # list_display = ('id', 'name', 'category', 'brand', 'gender', 'total_reviews')
+    # list_display_links = ('name', )
+    inlines = [ProductItemInline]
+    # inlines = [ReviewInline]
 
+    # def total_reviews(self, obj):
+    #     if obj.product_rating:
+    #         return round(obj.product_rating, 1)
+    #     else:
+    #         return 0
+    #
+    # total_reviews.short_description = 'Reviews'
+
+    def get_queryset(self, request):
+
+        queryset = Product.objects.select_related(
+            'category',
+            'brand'
+        )
+
+        return queryset
 
 # @admin.register(ProductItem)
 # class ProductItemAdmin(admin.ModelAdmin):
