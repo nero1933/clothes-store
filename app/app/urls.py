d7d3c0f8f8788727193fc9864a039ebd19b8e79b
nero1933 <neroigo.1933@gmail.com>
2025-02-01 18:06:18 +0200

    UPDATE: Build Fix

--- app/app/urls.py
+++ app/app/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/v1/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
-    path('_nested_admin/', include('nested_admin.urls')),
     path('', include('ecommerce.urls')),
 ]
 
