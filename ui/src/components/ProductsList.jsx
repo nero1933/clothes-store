0bdcecb99fdd6c3e5e7cc00271400da02915db39
nero1933 <neroigo.1933@gmail.com>
2025-03-16 16:16:00 +0200

    UPDATE: React

---
+++ ui/src/components/ProductsList.jsx
@@ -0,0 +1,12 @@
+// import React, { useEffect, useState } from "react";
+// import axios from 'axios';
+//
+//
+// export function ProductsList() {
+//     const [products, setProducts] = useState([]);
+//     const [loading, setLoading] = useState(true);
+//
+//     useEffect(() => {
+//         axios.get("/api/products")
+//     })
+// }
