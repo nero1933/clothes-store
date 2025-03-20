a82896b70aa4bfc7a9c0395f3caf6a4d3bba2590
nero1933 <neroigo.1933@gmail.com>
2025-03-20 10:48:13 +0200

    UPDATE: Auth (js)

--- ui/src/components/Footer.jsx
+++ ui/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return(
         <footer className="footer">
-            <div className="container">1</div>
             <div className="container">2</div>
+            <div className="container">1</div>
         </footer>
     )
 }
